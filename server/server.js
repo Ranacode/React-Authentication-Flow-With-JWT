d268710ac529f9aa89ecf2b9b685bfa05550eadd
Ranacode <romerodeveloper@techie.com>
2017-12-24 13:05:06 +0000

    removed useless files on client && config express

---
+++ server/server.js
@@ -0,0 +1,8 @@
+import app from "./config/express";
+
+const PORT = process.env.PORT || 8001;
+
+app.listen(PORT, error => {
+  if (error) console.error(`INTERNAL SERVER ERROR: ${error}`);
+  console.log(`Server listening on port: ${PORT}...`);
+});
