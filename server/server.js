0da5d92500d0351c52d38f321d87aca19d46cd3e
Ranacode <romerodeveloper@techie.com>
2017-12-24 13:15:35 +0000

    configuring babel and eslint

--- server/server.js
+++ server/server.js
@@ -1,8 +1,8 @@
-import app from "./config/express";
+import app from './config/express';
 
 const PORT = process.env.PORT || 8001;
 
-app.listen(PORT, error => {
+app.listen(PORT, (error) => {
   if (error) console.error(`INTERNAL SERVER ERROR: ${error}`);
   console.log(`Server listening on port: ${PORT}...`);
 });
